66cabe6506b9c113ffe305ca8d143fbb69aa110b
Sharif Uddin <sharifuddin6@outlook.com>
2015-12-09 20:37:38 +0000

    Web service allows consumption of methods related to product database.

---
+++ Web-Service-Project/IInventoryService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace Web_Service_Project
+{
+    [ServiceContract]
+    public interface IInventoryService
+    {
+        [OperationContract]
+        Product GetProduct(int productId);
+
+        [OperationContract]
+        List<Product> GetAllProducts();
+
+        [OperationContract]
+        bool AddProduct(Product product);
+
+        [OperationContract]
+        bool RemoveProduct(int productId);
+    }
+
+    [DataContract]
+    public class Product
+    {
+        [DataMember]
+        public int ProductId { get; set; }
+
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string Author { get; set; }
+
+        [DataMember]
+        public string Abstract { get; set; }
+
+        [DataMember]
+        public string Content { get; set; }
+
+        [DataMember]
+        public string Thumbnail { get; set; }
+
+        [DataMember]
+        public DateTime DateAdded { get; set; }
+    }
+}
---
+++ Web-Service-Project/InventoryService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Web_Service_Project.Entity;
+
+namespace Web_Service_Project
+{
+    public class InventoryService : IInventoryService
+    {
+        public Product GetProduct(int productId)
+        {
+            using (var context = new EntityContainer())
+            {
+                try
+                {
+                    var product = context.pProducts.Single(p => p.ProductId == productId);
+                    return new Product
+                    {
+                        ProductId = product.ProductId,
+                        Title = product.Title,
+                        Author = product.Author,
+                        Abstract = product.Abstract,
+                        Content = product.Content,
+                        Thumbnail = product.Thumbnail,
+                        DateAdded = product.DateAdded
+                    };
+                }
+                catch (InvalidOperationException e)
+                {
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            using (var context = new EntityContainer())
+            {
+                return Enumerable.Cast<Product>(context.pProducts.Select(p => new Product()
+                {
+                    ProductId = p.ProductId,
+                    Title = p.Title,
+                    Author = p.Author,
+                    Abstract = p.Abstract,
+                    Content = p.Content,
+                    Thumbnail = p.Thumbnail
+                })).ToList();
+            }
+        }
+
+        public bool AddProduct(Product product)
+        {
+            using (var context = new EntityContainer())
+            {
+                try
+                {
+                    context.pProducts.Add(new pProduct
+                    {
+                        Title = product.Title,
+                        Author = product.Author,
+                        Abstract = product.Abstract,
+                        Content = product.Content,
+                        Thumbnail = product.Thumbnail,
+                        DateAdded = product.DateAdded
+                    });
+                    context.SaveChanges();
+                    return true;
+                }
+                catch (InvalidOperationException e)
+                {
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+
+        public bool RemoveProduct(int productId)
+        {
+            using (var context = new EntityContainer())
+            {
+                try
+                {
+                    var product = context.pProducts.Single(p => p.ProductId == productId);
+                    context.pProducts.Remove(product);
+                    context.SaveChanges();
+                    return true;
+                }
+                catch (InvalidOperationException e)
+                {
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+    }
+}
